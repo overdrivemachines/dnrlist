030b121cec70dd657c4d1b2334cfcea374f024f6
get.dipen@gmail.com <get.dipen@gmail.com>
2022-09-26 14:27:10 -0700

    added back to top button

--- app/javascript/my_script.js
+++ app/javascript/my_script.js
@@ -15,6 +15,8 @@ window.$ = $;
 
 // This function runs on every page "load"
 document.addEventListener("turbo:load", () => {
+
+
   // list.js needs to be initialized
   let options = {
     valueNames: ["name", "dob"]
@@ -27,9 +29,36 @@ document.addEventListener("turbo:load", () => {
   const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
   const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
+
+  // Return to top button:
+  const returnToTopBtn = $("#return-to-top");
+
+  // When the user scrolls down 520px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 520 || document.documentElement.scrollTop > 520) {
+      returnToTopBtn.style.display = "block";
+    } else {
+      returnToTopBtn.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function returnToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+  window.returnToTop = returnToTop;
+
 });
 
 // This function runs on every turbo frame
 document.addEventListener("turbo:frame-load", function (e) {
   console.log("frame load", Math.random());
 });
+
+
+
+
