eef5262cbb3bf91a802139589f28663af06d1d7e
get.dipen@gmail.com <get.dipen@gmail.com>
2022-09-27 19:31:26 -0700

    added mailer

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: guests
+#
+#  id         :integer          not null, primary key
+#  dob        :date             not null
+#  firstname  :string(25)       not null
+#  lastname   :string(25)       not null
+#  reason     :string(50)       not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Guest < ApplicationRecord
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
     confirmations: 'users/confirmations'
   }
   root "guests#index"
