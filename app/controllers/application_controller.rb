adaf0e4fdf537f472a9db99dbc494297e54dc1c0
get.dipen@gmail.com <get.dipen@gmail.com>
2022-09-28 20:46:47 -0700

    Specifying url after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  # After signing out path
+  def after_sign_out_path_for(resource_or_scope)
+    flash[:notice] = "You have been signed out"
+    static_pages_message_path
+  end
 end
