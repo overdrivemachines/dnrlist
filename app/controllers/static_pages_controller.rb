3a4cbdbcb1ed10752761b33b2c9b8d43aab44658
get.dipen@gmail.com <get.dipen@gmail.com>
2022-09-28 20:47:30 -0700

    If there is no message redirect to root

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def message
+    # if there is no message redirect to root
+    if !flash[:notice]
+      redirect_to root_url
+    end
   end
 end
