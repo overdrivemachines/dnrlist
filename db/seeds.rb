eeebad3ef45559f25481807e4e5795a5cef2703f
get.dipen@gmail.com <get.dipen@gmail.com>
2023-03-04 17:30:45 -0800

    adding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-50.times do
-  Guest.create(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, dob: Faker::Date.birthday(min_age: 18, max_age: 65), reason: Faker::Company.bs, user_id: User.first.id)
+def create_guests
+  50.times do
+    Guest.create(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name,
+                 dob: Faker::Date.birthday(min_age: 18, max_age: 65), reason: Faker::Company.bs, user_id: User.first.id)
+  end
 end
+
+def create_guests_from_file
+  i = 1
+  File.foreach(Rails.root.join('db', 'in.txt')) do |line|
+    arr = line.strip.split(", ")
+    puts(i.to_s + " " + arr.to_s) if arr.size != 2
+    i += 1
+
+    # puts arr.to_s
+
+    Guest.create!(firstname: arr.last, lastname: arr.first, reason: "Not recorded", user_id: 2)
+  end
+end
+
+create_guests_from_file
